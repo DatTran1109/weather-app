adb313efa7da3a790cb5ce590f1748844912e60a
DatTran1109 <trannguyenthanhdat104@gmail.com>
2023-11-24 15:28:39 +0700

    first commit

---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String API_KEY = '1f68dd0bb6c03ecb943615dc735905d1';
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/bloc/weather_bloc.dart';
+import 'package:weather_app/screens/home_screen.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+        future: _determinePosition(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return BlocProvider<WeatherBloc>(
+              create: (context) => WeatherBloc()
+                ..add(FetchWeatherEvent(snapshot.data as Position)),
+              child: const HomeScreen(),
+            );
+          } else if (snapshot.hasError) {
+            return Scaffold(
+              body: Center(child: Text(snapshot.error.toString())),
+            );
+          } else {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error(
+        'Location services are disabled. Please enable services and try again.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error(
+          'Location permissions are denied. Please restart app and try again.');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
